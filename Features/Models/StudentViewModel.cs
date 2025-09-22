64166d8b15be8d1edac6c2b8db4264400ea53413
Gerardo Farias Blanco <gerarfarias11@gmail.com>
2025-09-21 20:11:44 -0400

    Update StudentViewModel.cs

--- Features/Models/StudentViewModel.cs
+++ Features/Models/StudentViewModel.cs
@@ -1,1 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
 
